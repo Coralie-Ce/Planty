1922597ed78398e8b974b97979859d47486b09c4
Coralie Cellier <coralie.cellier@outlook.fr>
2024-06-15 13:16:39 +0200

    modifications css accueil

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -4,5 +4,6 @@ add_action('wp_enqueue_scripts','theme_enqueue_style');
 function theme_enqueue_style()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('enfant-style', get_template_directory_uri() . '/css/theme.css');
 }
 ?>
